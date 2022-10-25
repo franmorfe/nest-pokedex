1d616638705fffbf4807360c222f2c717d74678a
Francisco J. Moreno <franmorfe@gmail.com>
2022-10-25 16:27:17 +0200

    Applying adapter pattern for http request

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
+  
 
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ){}
   
   async executeSeed() {
 
     await this.pokemonModel.deleteMany();
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
-
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
     const pokemonToInsert: { name: string, no: number }[] = [];
 
     data.results.forEach(
@@ -29,7 +29,6 @@ export class SeedService {
         const segments = url.split('/');
         const no: number = +segments[segments.length - 2];
 
-        //const pokemon = await this.pokemonModel.create({ name, no })
         pokemonToInsert.push({ name, no });
       }
     )
